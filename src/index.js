da30a33eaa52b479f90170eecbdd0ca2c5697fc6
Wesley <fernandess.weslley@gmail.com>
2022-05-02 21:47:24 -0300

    Start refactoring

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function checksExistsUserAccount(request, response, next) {
 
   const user = users.find((user) => user.username === username);
   if(!user) {
-    return response.status(400).json({error: "User not found!"})
+    return response.status(404).json({error: "User not found!"})
   };
 
   request.user = user;
@@ -32,14 +32,15 @@ app.post('/users', (request, response) => {
     return response.status(400).json({ error: "User already exists!"})
   };
 
-  users.push({
+  const newUser = {
     id: uuidv4(),
     name,
     username,
     todos: []
-  });
+  }
+  users.push(newUser);
 
-  return response.status(201).json({ success: "User created!"});
+  return response.status(201).json(newUser);
 
 });
 
@@ -48,7 +49,7 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 
   const user = users.find((user) => user.username === username);
 
-  return response.json(user);
+  return response.json(user.todos);
 
 });
 
